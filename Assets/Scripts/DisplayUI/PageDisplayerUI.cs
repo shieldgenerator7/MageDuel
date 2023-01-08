d35506cade0e3c8435975512a00ea6f6a8e3d63c
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-01-08 18:20:52 -0500

    Add PageDisplayerUI

---
+++ Assets/Scripts/DisplayUI/PageDisplayerUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageDisplayerUI : PlayerDisplayUI
+{
+    public Spell spell;
+
+    public List<Image> imgColors;
+    public TMP_Text txtName;
+    public TMP_Text txtDescription;
+
+    protected override void _registerDelegates(bool register)
+    {
+        Debug.LogWarning("Not implemented in PageDisplayerUI");
+    }
+
+    public override void forceUpdate()
+    {
+        imgColors.ForEach(img => img.color = spell.element.color);
+        txtName.text = spell.name;
+        txtDescription.text = spell.description;
+    }
+
+}
