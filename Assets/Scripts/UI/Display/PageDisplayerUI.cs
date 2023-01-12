03e4a6e5b6ac319f07a0261c8c0ef0d3d394f131
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-01-12 01:05:57 -0500

    Add PageCircles

--- Assets/Scripts/UI/Display/PageDisplayerUI.cs
+++ Assets/Scripts/UI/Display/PageDisplayerUI.cs
@@ -9,20 +9,35 @@ public class PageDisplayerUI : PlayerDisplayUI
     public int pageIndex;
     private Spell spell;
 
-    public List<Image> imgColors;
     public TMP_Text txtName;
     public TMP_Text txtDescription;
+    public List<Image> imgColors;
+    public List<GameObject> goToggleList;
+
+    private bool pageExists = true;
 
     protected override void _registerDelegates(bool register)
     {
-        spell = player.deck[pageIndex];
+        pageExists = pageIndex < player.deck.spellList.Count;
+        if (pageExists)
+        {
+            spell = player.deck[pageIndex];
+        }
+        gameObject.SetActive(pageExists);
     }
 
     public override void forceUpdate()
     {
+        if (!pageExists) { return; }
         imgColors.ForEach(img => img.color = spell.element.color);
         txtName.text = spell.name;
         txtDescription.text = spell.Description;
+        showToggles(false);
+    }
+
+    public void showToggles(bool show)
+    {
+        goToggleList.ForEach(go => go.SetActive(show));
     }
 
 }
