c48176cdd1110a58228fdfe34eff84fb823a9ace
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-01-15 20:02:50 -0500

    Add heal anim

--- Assets/Scripts/UI/Display/PlayerPoolDisplayer.cs
+++ Assets/Scripts/UI/Display/PlayerPoolDisplayer.cs
@@ -35,40 +35,66 @@ public class PlayerPoolDisplayer : PlayerDisplayUI
     private void updateHealthBar(int health)
     {
         float percent = (float)health / (float)player.health.maxValue;
-        //Instant update
-        healthBar.fillAmount = percent;
-        //Animation
-        Animation healthAnim = new Animation(imgHealthDiff, percent);
-        queueAnimation(healthAnim);
+        //Show damage anim
+        if (percent < healthBar.fillAmount)
+        {
+            //Instant update
+            healthBar.fillAmount = percent;
+            //Animation
+            Animation healthAnim = new Animation(imgHealthDiff, percent);
+            queueAnimation(healthAnim);
+        }
+        //Show heal anim
+        else
+        {
+            //Instant update
+            imgHealthDiff.fillAmount = percent;
+            //Animation
+            Animation healthAnim = new Animation(healthBar, percent);
+            queueAnimation(healthAnim);
+        }
     }
 
     private void updateAuraBar(int aura)
     {
-        float percent = (float)aura / (float)player.aura.maxValue;
-        //Instant update
-        auraBar.fillAmount = percent;
-        //Animation
-        Animation auraAnim = new Animation(imgAuraDiff, percent);
-        queueAnimation(auraAnim);
+        float percent = (float)aura / (float)player.aura.maxValue; //Show damage anim
+        //Show damage anim
+        if (percent < healthBar.fillAmount)
+        {
+            //Instant update
+            auraBar.fillAmount = percent;
+            //Animation
+            Animation auraAnim = new Animation(imgAuraDiff, percent);
+            queueAnimation(auraAnim);
+        }
+        //Show heal anim
+        else
+        {
+            //Instant update
+            imgAuraDiff.fillAmount = percent;
+            //Animation
+            Animation auraAnim = new Animation(auraBar, percent);
+            queueAnimation(auraAnim);
+        }
     }
 
     private void queueAnimation(Animation anim)
     {
         //Start anim after prev anim finishes
         if (animationQueue.Count > 0)
-        {//
+        {
             animationQueue.Peek().onFinished += () =>
                 Managers.Animation.startAnimation(anim, 1);
-        }//
+        }
         //Start anim now
-        else//
-        {//
+        else
+        {
             Managers.Animation.startAnimation(anim, 1);
-        }//
+        }
         //Put anim in queue
         animationQueue.Enqueue(anim);
         anim.onFinished += () => animationQueue.Dequeue();//assumes this anim is at start of queue
-    }//
+    }
 
     private void updateFocusBar(int focus)
     {
